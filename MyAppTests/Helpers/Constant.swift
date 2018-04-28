5690579f73249ce6fb0a58f855e4ce4448a158b1
Lam Le V <vanlam1503@gmail.com>
2018-04-28 11:03:24 +0700

    Unit test api core

---
+++ MyAppTests/Helpers/Constant.swift
@@ -0,0 +1,11 @@
+//
+//  Helpers.swift
+//  BBCap
+//
+//  Created by Le Van Lam on 4/28/18.
+//  Copyright © 2018 Asian Tech Co., Ltd. All rights reserved.
+//
+
+import Foundation
+
+let timeOut: TimeInterval = 20
