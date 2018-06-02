cf9cb98e6ce2266dffd7a98b24859513968d629c
Lam Le V <vanlam1503@gmail.com>
2018-06-02 21:06:25 +0700

    Add api link

--- BBCap/Model/API/Core/Api.swift
+++ BBCap/Model/API/Core/Api.swift
@@ -7,19 +7,24 @@
 //
 
 import Foundation
+import SwifterSwift
 
 final class Api {
 
     struct Path {
         #if DEBUG
-            static let baseURL = "https://dev-asiantech.vn"
+        static let baseURL = "https://dev-asiantech.vn"
         #elseif STG
-            static let baseURL = "https://stg-asiantech.vn"
+        static let baseURL = "https://stg-asiantech.vn"
         #else
-            static let baseURL = "https://pro-asiantech.vn"
+        static let baseURL = "https://pro-asiantech.vn"
         #endif
 
         static let v1 = "api" / "v1"
+    }
+
+    struct CoinmarketCap {
+        static let path = "https://graphs2.coinmarketcap.com"
     }
 }
 
@@ -29,6 +34,27 @@ extension Api.Path {
     struct Users {
 
         static let path = baseURL / v1 / "users"
+    }
+}
+
+extension Api.CoinmarketCap {
+
+    struct Currencies {
+        static let path = Api.CoinmarketCap.path / "currencies"
+    }
+}
+
+extension Api.CoinmarketCap.Currencies {
+
+    struct Bitcoin {
+
+        var startDate: TimeInterval
+        var endDate: TimeInterval
+
+        var path: String {
+            return Api.CoinmarketCap.Currencies.path / "bitcoin" / startDate.int / endDate.int
+        }
+
     }
 }
 
@@ -48,6 +74,10 @@ extension Int: URLStringConvertible {
     var urlString: String { return String(describing: self) }
 }
 
+extension Double: URLStringConvertible {
+    var urlString: String { return String(describing: self) }
+}
+
 private func / (lhs: URLStringConvertible, rhs: URLStringConvertible) -> String {
     return lhs.urlString + "/" + rhs.urlString
 }
@@ -59,3 +89,4 @@ extension String: URLStringConvertible {
 extension CustomStringConvertible where Self: URLStringConvertible {
     var urlString: String { return description }
 }
+
